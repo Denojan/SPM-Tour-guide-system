329575c8567636332cecca479b8a227aabb4e4e9
IT21181856 <it21181856@my.sliit.lk>
2023-09-07 20:11:22 +0530

    wip 4

--- tourmate/frontend/src/component/WishList.jsx
+++ tourmate/frontend/src/component/WishList.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import { Link } from "react-router-dom";
 import NavBar from "./NavBar";
-// import { CgNotes } from 'react-icons/cg';
+import { CgNotes } from 'react-icons/cg';
 
 function Wishlist() {
   const [placesData, setPlacesData] = useState([]);
@@ -114,10 +114,10 @@ function Wishlist() {
                         </Link>
 
                         <button type="button"
-                        class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                     
                         >
-                          {/* <CgNotes style={{ fontSize: '2.3rem' }} title="Note" /> */}
-                          Note
+                          <CgNotes style={{ fontSize: '2.3rem' }} title="Note" />
+                          {/* Note */}
                         </button>
                       </div>
                     </div>
