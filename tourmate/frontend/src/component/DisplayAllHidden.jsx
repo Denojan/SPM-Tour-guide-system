2101ff195f1f822e92e5af5db1b63cad6d90ecd7
sandarujayathilaka <sandarujayathilaka26@gmail.com>
2023-10-12 07:56:05 +0530

    update loading

--- tourmate/frontend/src/component/DisplayAllHidden.jsx
+++ tourmate/frontend/src/component/DisplayAllHidden.jsx
@@ -6,42 +6,25 @@ import { Link } from 'react-router-dom';
 function DisplayAllHidden() {
   const [favoritePlaces, setFavoritePlaces] = useState([]);
   const [selectedPlace, setSelectedPlace] = useState(null);
-  const [dropdownIndex, setDropdownIndex] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    async function fetchFavoritePlaces() {
-      try {
-        const response = await axios.get(
-          "http://localhost:8080/favplace/gethidden"
-        );
-        console.log(response);
-        setFavoritePlaces(response.data.place);
-      } catch (error) {
-        console.error("Error fetching favorite places:", error);
-      }
-    }
-
-    fetchFavoritePlaces();
-  }, []);
-
-  const handleDeleteClick = async (placeId) => {
-    const confirmed = window.confirm(
-      "Are you sure you want to delete this place?"
-    );
-    if (!confirmed) {
-      return;
-    }
-
+useEffect(() => {
+  async function fetchFavoritePlaces() {
+    setIsLoading(true); // Set loading state to true
     try {
-      const response = await axios.delete(
-        `http://localhost:8080/favplace/deleteplace/${placeId}`
+      const response = await axios.get(
+        "http://localhost:8080/favplace/gethidden"
       );
-      console.log("Place deleted:", response.data);
-      // Refresh the list after successful deletion
+      setFavoritePlaces(response.data.place);
     } catch (error) {
-      console.error("Error deleting place:", error);
+      console.error("Error fetching favorite places:", error);
+    } finally {
+      setIsLoading(false); // Set loading state to false 
     }
-  };
+  }
+
+  fetchFavoritePlaces();
+}, []);
 
   const handleTextSearch = (e) => {
     const searchTerm = e.currentTarget.value;
@@ -67,9 +50,6 @@ function DisplayAllHidden() {
     setSelectedPlace(null);
   };
 
-  const toggleDropdown = (index) => {
-    setDropdownIndex(index === dropdownIndex ? null : index);
-  };
 
   return (
     <div className="pb-10">
@@ -115,89 +95,112 @@ function DisplayAllHidden() {
           </div>
         </form>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-36 m-8">
-        {favoritePlaces.length === 0 ? (
-          <div className="bg-red-600">
-            <h1 className="text-center text-zinc-50">No Records Found</h1>
-          </div>
-        ) : (
-          favoritePlaces.map((place, index) => (
-            <div
-              key={index}
-              className="bg-white shadow-md rounded-lg overflow-hidden transition duration-300 transform hover:-translate-y-1 hover:shadow-xl relative"
+      {isLoading ? (
+        <div className="text-center flex justify-center items-center mt-36 mb-20">
+          <div role="status">
+            <svg
+              aria-hidden="true"
+              class="w-12 h-12 mr-2  text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <Link onClick={() => openPlaceDetails(place)}>
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+            <span class="sr-only">Loading...</span>
+          </div>
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-36 m-8">
+          {favoritePlaces.length === 0 ? (
+            <div className="bg-red-600">
+              <h1 className="text-center text-zinc-50">No Records Found</h1>
+            </div>
+          ) : (
+            favoritePlaces.map((place, index) => (
+              <div
+                key={index}
+                className="bg-white shadow-md rounded-lg overflow-hidden transition duration-300 transform hover:-translate-y-1 hover:shadow-xl relative"
+              >
+                <Link onClick={() => openPlaceDetails(place)}>
+                  <img
+                    src={place.image}
+                    alt={place.placeName}
+                    className="w-full h-40 object-cover"
+                  />
+                </Link>
+                <div className="p-4">
+                  <h5 className="text-3xl font-semibold mb-2 text-gray-800">
+                    {place.placeName}
+                  </h5>
+                </div>
+              </div>
+            ))
+          )}
+          {selectedPlace && (
+            <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50">
+              <div className="bg-white p-8 rounded-lg max-w-screen-sm">
                 <img
-                  src={place.image}
-                  alt={place.placeName}
+                  src={selectedPlace.image}
+                  alt={selectedPlace.placeName}
                   className="w-full h-40 object-cover"
                 />
-              </Link>
-              <div className="p-4">
-                <h5 className="text-3xl font-semibold mb-2 text-gray-800">
-                  {place.placeName}
-                </h5>
-               
-              </div>
-            </div>
-          ))
-        )}
-        {selectedPlace && (
-          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50">
-            <div className="bg-white p-8 rounded-lg max-w-screen-sm">
-              <img
-                src={selectedPlace.image}
-                alt={selectedPlace.placeName}
-                className="w-full h-40 object-cover"
-              />
-              <h2 className="text-3xl font-semibold mb-2 text-gray-800">
-                {selectedPlace.placeName}
-              </h2>
-              <span></span>
-              <p className="text-lg font-semibold text-blue-950">
-                Location:
-                <span className="ml-3 text-lg text-sky-700">
-                  {selectedPlace.location}
-                </span>
-              </p>
-              <span></span>
-              <p className="text-lg font-semibold text-blue-950">
-                Contact:{" "}
-                <span className="ml-3 text-lg text-sky-700">
-                  {selectedPlace.contact}
-                </span>
-              </p>
-              <span></span>
-              <span></span>
-              <p className="text-lg font-semibold text-blue-950">
-                Category:{" "}
-                <span className=" ml-3 text-lg text-sky-700">
-                  {selectedPlace.category}
-                </span>
-              </p>
-              <span></span>
-              <p className="text-lg font-semibold text-blue-950">
-                Description:{" "}
-              </p>
-              <br></br>
-              <textarea className="w-96 h-40 text-lg" disabled>
-                {selectedPlace.description}
-              </textarea>
+                <h2 className="text-3xl font-semibold mb-2 text-gray-800">
+                  {selectedPlace.placeName}
+                </h2>
+                <span></span>
+                <p className="text-lg font-semibold text-blue-950">
+                  Location:
+                  <span className="ml-3 text-lg text-sky-700">
+                    {selectedPlace.location}
+                  </span>
+                </p>
+                <span></span>
+                <p className="text-lg font-semibold text-blue-950">
+                  Contact:{" "}
+                  <span className="ml-3 text-lg text-sky-700">
+                    {selectedPlace.contact}
+                  </span>
+                </p>
+                <span></span>
+                <span></span>
+                <p className="text-lg font-semibold text-blue-950">
+                  Category:{" "}
+                  <span className=" ml-3 text-lg text-sky-700">
+                    {selectedPlace.category}
+                  </span>
+                </p>
+                <span></span>
+                <p className="text-lg font-semibold text-blue-950">
+                  Description:{" "}
+                </p>
+                <br></br>
+                <textarea className="w-96 h-40 text-lg" disabled>
+                  {selectedPlace.description}
+                </textarea>
 
-              <div className="mt-4 flex justify-center">
-                {" "}
-                {/* Center the button */}
-                <button
-                  className="px-4 py-2 text-lg bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors duration-300"
-                  onClick={closePlaceDetails}
-                >
-                  Close
-                </button>
+                <div className="mt-4 flex justify-center">
+                  {" "}
+                  {/* Center the button */}
+                  <button
+                    className="px-4 py-2 text-lg bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors duration-300"
+                    onClick={closePlaceDetails}
+                  >
+                    Close
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        )}
-      </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
